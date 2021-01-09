fbc13c86cdd77df66f5cb96daf8fa2f1fdb3d648
Andy Grove <andygrove73@gmail.com>
2021-01-09 11:16:44 -0700

    Add docs and convenience methods

--- rust/ballista-proto/src/lib.rs
+++ rust/ballista-proto/src/lib.rs
@@ -1,7 +1,90 @@
+// Copyright 2020 Andy Grove
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! This crate contains code generated from the Ballista Protocol Buffer Definition as well
+//! as convenience code for interacting with the generated code.
+
+// include the generated protobuf source as a submodule
+#[allow(clippy::all)]
+pub mod protobuf {
+    include!(concat!(env!("OUT_DIR"), "/ballista.protobuf.rs"));
+}
+
+pub const BALLISTA_PROTO_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+/// Create an empty ExprNode
+pub fn empty_expr_node() -> protobuf::LogicalExprNode {
+    protobuf::LogicalExprNode {
+        alias: None,
+        column_name: "".to_owned(),
+        has_column_name: false,
+        literal_string: "".to_owned(),
+        has_literal_string: false,
+        literal_int: 0,
+        literal_uint: 0,
+        literal_f32: 0.0,
+        literal_f64: 0.0,
+        has_literal_i8: false,
+        has_literal_i16: false,
+        has_literal_i32: false,
+        has_literal_i64: false,
+        has_literal_u8: false,
+        has_literal_u16: false,
+        has_literal_u32: false,
+        has_literal_u64: false,
+        has_literal_f32: false,
+        has_literal_f64: false,
+        binary_expr: None,
+        aggregate_expr: None,
+    }
+}
+
+/// Create an empty LogicalPlanNode
+pub fn empty_logical_plan_node() -> protobuf::LogicalPlanNode {
+    protobuf::LogicalPlanNode {
+        scan: None,
+        input: None,
+        projection: None,
+        selection: None,
+        limit: None,
+        aggregate: None,
+    }
+}
+
+/// Create an empty PhysicalPlanNode
+pub fn empty_physical_plan_node() -> protobuf::PhysicalPlanNode {
+    protobuf::PhysicalPlanNode {
+        scan: None,
+        input: None,
+        projection: None,
+        selection: None,
+        global_limit: None,
+        local_limit: None,
+        shuffle_reader: None,
+        hash_aggregate: None,
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
+    use crate::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn sanity_check() {
+        let _ = empty_logical_plan_node();
+        let _ = empty_expr_node();
+        let _ = empty_physical_plan_node();
     }
 }
